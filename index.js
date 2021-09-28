fa131aa3a8174fb4179643a79d515cc88d5323d6
Guilherme Garcia <guilherme4garcia@gmail.com>
2021-09-28 04:09:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request')
 require('dotenv/config')
 
 
-const url_sjc = 'http://api.openweathermap.org/data/2.5/weather?q=Sao Paulo&units=metric&appid=89ce5587ceaaa30876e3d3e3dfac2351'
+
 const url = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
 const key = process.env.KEY
 
